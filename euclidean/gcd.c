6cc48f12a3ac8f1a12c1fa03c80fbb40913e02e4
duyanrong <duyanrong900@163.com>
2017-03-03 19:56:29 +0800

    euclidea , opj1749(unaccepte）

---
+++ euclidean/gcd.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+	reurn b ? gcd(b,a%b) : a;
+}
+
+/**
+* ax + by = gcd(a,b)
+*/
+int ext_gcd(int a, int b, int x, int y)
+{
+	if(b == 0)
+	{
+		x = 1; y =0;
+		return a;
+	}
+	int q = ext_gcd(b, a%b, x, y);
+	y -= a/b*x;
+	return q;
+}
+
+/*
+*ax + by = gcd(a, b) = d
+*/
+
+int ext_gcd2(int a, int b, int x, int y)
+{
+	if(b == 0)
+	{
+		x = 1; y =0;
+		return a;
+	}
+
+	int c = ext_gcd2(b, a%b, x, y);
+	int t = x;
+	x = y; y = t - a/b*y;
+	return c;
+}
