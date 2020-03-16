3bcd763e9d8e788e92f679a77e824f067ef34c77
dyanrong <dyanrong@unimaxcomm.com>
2020-03-16 18:16:06 +0800

    update leet-10 offical resolution
    Change-Id: I9ba3ec37eeb156e9e088e6a6cf8aee575cc4e0af

--- leetcode/leet-10.cc
+++ leetcode/leet-10.cc
@@ -2,23 +2,23 @@
 #include <string>
 #include <vector>
 using namespace std;
-//dp[i][j]表示s [0, i-1]与p[0, j-1]是否匹配
+//dp[i][j] s[0, i-1] is match with p[0, j-1]
 class Solution2 {
 public:
     bool isMatch(string s, string p) {
         int l1 = s.size();
         int l2 = p.size();
         vector<vector<bool>> dp(l1+1,vector<bool>(l2+1,false));
-        dp[0][0] = true;//只有空串才能匹配空串
-        for(int j=1;j<=l2;j++){
-            dp[0][j] = j>1 && p[j-1]=='*'&&dp[0][j-2];
+        dp[0][0] = true;//empty string just match itself
+        for(int j = 1; j <= l2; j++){
+            dp[0][j] = j > 1 && p[j-1] =='*'&&dp[0][j-2];
         }
-        for(int i=1;i<=l1;i++){
-            for(int j=1;j<=l2;j++){
-                if(p[j-1]!='*'){
-                    dp[i][j] = (s[i-1]==p[j-1]||p[j-1]=='.') && dp[i-1][j-1];
+        for(int i = 1; i <= l1; i++){
+            for(int j = 1; j <= l2; j++){
+                if(p[j-1] != '*'){
+                    dp[i][j] = (s[i-1] == p[j-1]||p[j-1] == '.') && dp[i-1][j-1];
                 }else{
-                    dp[i][j] = (j>1&&dp[i][j-2]) || (s[i-1]==p[j-2]||p[j-2]=='.')&&dp[i-1][j];//这里i-1相当于递归时的i+1
+                    dp[i][j] = (j > 1 && dp[i][j-2]) || (s[i-1] == p[j-2] || p[j-2] == '.') && dp[i-1][j];//这里i-1相当于递归时的i+1
                 }
             }
         }
@@ -29,10 +29,10 @@ public:
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.length(),n = p.length();
+        int m = s.length(), n = p.length();
         bool dp[m+1][n+1] = {false};
         dp[0][0] = true;
-        //初始化第0行,除了[0][0]全为false，因为空串p只能匹配空串，其他都无能匹配
+        //is p is empty, only can match s
         for (int i = 1; i <= m; i++)
             dp[i][0] = false;
         //初始化第0列，只有X*能匹配空串
@@ -41,20 +41,33 @@ public:
         
         for (int i = 1; i <= m; i++)
         {
-           for (int j = 1; j <= n; j++)
-           {
-               if (p[j - 1] == '*')
-               {
-                   dp[i][j] = dp[i][j - 2] || (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j];
-               }
-               else //只有当前字符完全匹配，才能传递dp[i-1][j-1] 值
-               {
-                   dp[i][j] = (p[j - 1] == '.' || s[i - 1] == p[j - 1]) && dp[i - 1][j - 1];
-               }
-           }
+            for (int j = 1; j <= n; j++)
+            {
+                if (p[j - 1] == '*')
+                {
+                    dp[i][j] = dp[i][j - 2] || (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j];
+                }
+                else //只有当前字符完全匹配，才能传递dp[i-1][j-1] 值
+                {
+                    dp[i][j] = (p[j - 1] == '.' || s[i - 1] == p[j - 1]) && dp[i - 1][j - 1];
+                }
+            }
         }
         
         return dp[m][n];
+    }
+
+    bool isMatch(string s, string p) {
+        //empty patter only can mathched with empty origin.
+        if(p.empty()) return s.empty();
+
+        bool first_match = (!s.empty() && (p.at(0) == s.at(0) || p.at(0) == '.'));
+        if(p.size() >= 2 && p.at(1) == '*'){
+            return (isMatch(s, p.substr(2)) 
+                || first_match && isMatch(s.substr(1), p));
+        }else{
+            return first_match && isMatch(s.substr(1), p.substr(1));
+        }
     }
 };
 
