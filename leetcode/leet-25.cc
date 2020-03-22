5ab1288772f71af796761125369ccce556ca87d2
yanrong <yanrongdu@gmail.com>
2020-03-22 23:56:21 +0800

    update leetcode 25 solutions

--- leetcode/leet-25.cc
+++ leetcode/leet-25.cc
@@ -1,6 +1,4 @@
-/**
- * Definition for singly-linked list.
-**/
+/** Definition for singly-linked list. **/
 struct ListNode {
     int val;
     ListNode *next;
@@ -53,5 +51,61 @@ public:
         spost->val = temp;
 
         subReverse(lmark, rmark, k - 2);
+    }
+    
+    /**other solution**/
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head != nullptr && k == 1) return head;
+        
+        ListNode* dummy = new ListNode(-1);
+        ListNode *pre = dummy, *cur = head;
+        dummy->next = head;
+        
+        for(int i = 1; cur; i++){
+            if(i % k == 0){//at each reverse node, crate a function return reverse node
+                pre = reverse1Group(pre, cur->next);
+                cur = pre->next;
+            }else{//if it is not reverse node, skip to next
+                cur = cur->next;
+            }
+        }
+        
+        return dummy->next;
+    }
+    
+    ListNode* reverse1Group(ListNode* pre, ListNode* next){
+        ListNode* last = pre->next;// got i + k - 1 node
+        ListNode* cur = last->next; //get i + k node
+        
+        while(cur != next){
+            last->next = cur->next;
+            cur->next = pre->next;
+            pre->next = cur;
+            cur = last->next;
+        }
+        
+        return last;
+    }
+    
+    /***solution 3***/
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* pre = dummy, *cur = pre;
+        int num = 0;
+        dummy->next = head;
+        while(cur = cur->next) num++;
+        
+        while(num >= k){
+            cur = pre->next;
+            for(int i = 1 ; i < k; i++){
+                ListNode* t = cur->next;
+                cur->next = t->next;
+                t->next = pre->next;
+                pre->next = t;
+            }
+            pre = cur;
+            num -= k;
+        }
+        return dummy->next;
     }
 };
