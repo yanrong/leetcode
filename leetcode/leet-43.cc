ccbb33ea84c7e32e9426d9a627fb04bf502e68a4
yanrong <yanrongdu@gmail.com>
2020-04-01 23:50:37 +0800

    update leetcode-043 official code

--- leetcode/leet-43.cc
+++ leetcode/leet-43.cc
@@ -8,7 +8,7 @@ public:
     //chinese traditional arithmetic algo
     string multiply(string num1, string num2) {
         string ret = "", temp = "";
-        int carray, subCarray;
+        int carry, subCarry;
         long long t;
         
         if(num1.size() < num2.size()) {
@@ -16,13 +16,13 @@ public:
         }
         if(num2 == "0") return "0";
         for(int i = num2.size() - 1; i >= 0; i--){
-            temp = ""; carray = 0; t = 0;
+            temp = ""; carry = 0; t = 0;
             for(int j = num1.size() - 1; j >= 0; j--){
-                t = (num2[i] - '0') * (num1[j] - '0') + carray;
+                t = (num2[i] - '0') * (num1[j] - '0') + carry;
                 temp = to_string(t % 10) + temp ;
-                carray = t / 10;
+                carry = t / 10;
             }
-            if(carray) temp = to_string(carray) + temp;
+            if(carry) temp = to_string(carry) + temp;
 
             for(int k = num2.size() - 1; k > i; k--){
                 temp += "0";
@@ -31,15 +31,15 @@ public:
             if(ret.size() == 0){
                 ret = temp;
             }else{
-                subCarray = 0;
+                subCarry = 0;
                 for(int m = ret.size() - 1, n = temp.size() - 1; m >= 0 || n >= 0; m--, n--){
-                        t = ((m < 0 ? '0' : ret[m]) - '0') + (temp[n] - '0') + subCarray;
+                        t = ((m < 0 ? '0' : ret[m]) - '0') + (temp[n] - '0') + subCarry;
                         temp[n] = t % 10 + '0';
-                        subCarray = t / 10;
+                        subCarry = t / 10;
                 }
                 
-                if(subCarray){
-                    temp = to_string(subCarray) + temp;
+                if(subCarry){
+                    temp = to_string(subCarry) + temp;
                 }
             }
             ret = temp;
@@ -48,28 +48,60 @@ public:
     }
 
     //other solution provide by leetcode
+    /**
+    * each time select two number multiply and get result, if the index of two number in string
+    * is i and j, the result in a row array is i + j + 1; the index may be greater than 10, we
+    * just store the number in i + j and i + j + i.
+    **/
     string multiply(string num1, string num2) {
         vector<int> result(num1.size() + num2.size() + 1, 0);
-        string ret = "";
-        if (num1=="0" || num2=="0") return "0";
+        string ret;
+        int t1, t2, temp;
+        bool flag = true;
+        if (num1 == "0" || num2 == "0") return "0";
         
-        for (int i = 0; i < num1.size(); i++) {
-            for (int j = 0; j < num2.size(); j++) {
-                result[i+j+1] += (num1[i] - '0') * (num2[j] - '0');
+        for (int i = num1.size() - 1; i >= 0; i--) {
+            t1 = num1[i] - '0';
+            for (int j = num2.size() - 1; j >= 0; j--) {
+                t2 = num2[j] - '0';
+                //each time , add the number in i + j + 1 to current result 
+                temp = result[i + j + 1] + (t1 * t2);
+                result[i + j + 1] = temp % 10; // i + j + 1 keep the lsb
+                result[i + j] += temp / 10; // i + j keep the msb
             }
         }
-        for (int i = result.size() - 1; i > 0; i--) {
-            if (result[i] < 10) continue;
-            result[i-1] += result[i] / 10;
-            result[i] %= 10;
-        }
         
-        bool zero = true;
         for (int i = 0; i < result.size() - 1; i++) {
-            if (zero && result[i] == 0) continue;
-            zero = false;
-            ret.push_back(result[i]+'0');
+            if (flag && result[i] == 0) continue; // skip the zero before the first valid number
+            flag = false;
+            ret.push_back(result[i] + '0');
         }
         return ret;
+    }
+    //other solution in same way
+    string multiply(string num1, string num2) {
+        string r;
+        bool flag = true;
+        if (num1 == "0" || num2 == "0") return "0";
+        vector<int> result(num1.size() + num2.size() + 1, 0);
+        for (int i = 0; i < num1.size(); i++) {
+            for (int j = 0; j < num2.size(); j++) {
+                //first step, just calculate the whole multiply result
+                result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+            }
+        }
+        //now from the end to start
+        for (int i = result.size() - 1; i > 0; i--) {
+            if (result[i] < 10) continue; //if there is no need to calculate carry, skip it
+            result[i - 1] += result[i] / 10; //add the MSB
+            result[i] %= 10; // get carry integer by mod LSB
+        }
+        
+        for (int i = 0; i < result.size() - 1; i++) {
+            if (flag && result[i] == 0) continue;
+            flag = false;
+            r.push_back(result[i] + '0');
+        }
+        return r;
     }
 };
--- leetcode/leet-44.cc
+++ leetcode/leet-44.cc
@@ -6,20 +6,26 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         int m = s.length(), n = p.length();
-        bool dp[m+1][n+1] = {false};
-        if(m == 0 && n == 0) return true;
-        if(n == 0) return false;
-
+        //the substring before index i in s is matched with substring before index j in p
+        bool dp[m+1][n+1]={false};
+        if (m == 0 && n == 0) return true;
+        if (n == 0) return false;
+        //empty s and p is matched
         dp[0][0] = true;
-        for(int i = 1; i <= n; i++){
-           dp[0][i] =  p[i - 1] == '*' && dp[0][i -1];
+        for(int i = 1; i <= n; i++){//is string s is empty, only can match with one or more *
+            dp[0][i] = p[i - 1] == '*' && dp[0][i - 1] ;
         }
         
         for(int i = 1; i <= m; i++){
-            for(int j = 1; j<= n; j++){
+            //dp[i][0] = p[0] == '*' && dp[i-1][0];
+            for(int j = 1; j <= n; j++){
                 if(p[j - 1] == '*'){
+                    // '*' can match a empty or one more sequence, so this is 
+                    // '*' match one or more sequence or matched no one discard '*' 
                     dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
                 }else{
+                    //else if char at index is matched or the char in index is . in p,
+                    // it can be matched any char, and dp[i-1][j-1] is true
                     dp[i][j] = (p[j - 1] == s[i - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
                 }
             }
