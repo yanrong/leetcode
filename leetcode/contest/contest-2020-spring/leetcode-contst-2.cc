16edc9ef9335f85d3360e1691b5c89a54dd83d52
yanrong <yanrongdu@gmail.com>
2020-05-14 23:01:07 +0800

    udpate official contest code

--- leetcode/contest/contest-2020-spring/leetcode-contst-2.cc
+++ leetcode/contest/contest-2020-spring/leetcode-contst-2.cc
@@ -1,33 +1,54 @@
 #include <vector>
+#include <map>
+using std::map;
 using std::vector;
 
 class Solution {
 public:
-	//Answer Error
+    //leetcode official solution
     int numWays(int n, vector<vector<int>>& relation, int k) {
-        int m = relation.size();
-        int ret = 0, step;
+        /* dp[i][j] 表示数组的第 i 轮传递给编号 j 的人的方案数。
+        ** y 玩家的所有玩家编号 x1,x2,x3... , 则第 i+1 轮传递信息给编号 y 玩家的递推方程为
+        ** dp[i+1][y] = sum(dp[i][x1],dp[i][x2],dp[i][x3]...)，
+        ** 其递推形式即
+        ** dp[i+1][y] += dp[i][x]
+        */
+        vector<vector<int>> dp(k + 1, vector<int>(n));
         
-        vector<int> t;
-        
-        for(int i = 1; i < m; i++){
-            t = relation[0];
-            step = 0;
-            if(t[1] == relation[i][0]){
-                t = relation[i];
-                step++;
-                for(int j = 1; j < m && step < k; j++){
-                    if(t[1] == relation[j][0]){
-                        t = relation[j];
-                        step++;
-                    }    
-                }
-            }
-            if(step == k -1 &&  t[1] == n - 1){
-                ret++;
+        dp[0][0] = 1;
+        for(int i = 0; i < k; i++){
+            for(auto vec : relation){
+                dp[i + 1][vec[1]] += dp[i][vec[0]];
             }
         }
+        return dp[k][n - 1];
+    }
+};
+
+//leetcode genius guy DFS
+class Solution {
+public:
+    int ans = 0;
+    int numWays(int n, vector<vector<int>>& relation, int k) {
+        for(int i = 0; i < relation.size(); i++){
+            //in the beginning start at 0
+            if(relation[i][0] == 0)
+                dfs(relation, relation[i][1], 1, k, n);
+        }
         
-        return ret;
+        return ans;
+    }
+    
+    void dfs(vector<vector<int>>& relation, int start, int step, int k, int n){
+        if(step == k){
+            if(start == n-1)
+                ans++; // if in step k and we reach the n, add counter
+            return;
+        }
+        for(int i = 0; i < relation.size(); i++){
+            //from the start, search next one
+            if(relation[i][0] == start)
+                dfs(relation, relation[i][1], step+1, k, n);
+        }
     }
 };
