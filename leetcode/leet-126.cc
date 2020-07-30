4dc8046e4026ec58c442596bd907ed504f2849db
duyanrong <duyanrong900@163.com>
2020-07-30 20:31:59 +0800

    update leetcode-126 official code

--- leetcode/leet-126.cc
+++ leetcode/leet-126.cc
@@ -1,14 +1,15 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
-#include<queue>
+#include <queue>
+#include <climits>
+
 using std::queue;
 using std::unordered_map;
 using std::vector;
 using std::string;
 
 //leetcode official solution to learning
-const int INF = 1 << 20;
 class Solution {
 private:
     unordered_map<string, int> wordId;
@@ -20,7 +21,6 @@ public:
         int id = 0;
         vector<vector<string>> ret;
         queue<vector<int>> q;
-        vector<int> cost(id, INF);
         
         for(const string& word : wordList){
             //calculate the unique word in word list
@@ -41,37 +41,42 @@ public:
         //resize the edges
         edges.resize(idWord.size());
         //build the edges in id word list
-        for(int i = 0; i< idWord.size(); i++){
+        for(int i = 0; i < idWord.size(); i++){
             for(int j = i + 1; j < idWord.size(); j++){
                 if(transformCheck(idWord[i], idWord[j])){
-                    edges[i].push_back(i);
-                    edges[j].push_back(j);
+                    //i -> j ; j-> i
+                    edges[i].push_back(j);
+                    edges[j].push_back(i);
                 }
             }
         }
-
+        //mark the dest id
         const int dest = wordId[endWord];
-        q.push(vector<int>{wordId[beginWord]});
-        cost[wordId[beginWord]] = 0;
+        vector<int> cost(id, INT_MAX); // cost record the size from begin to id-string
+        q.push(vector<int>{wordId[beginWord]}); // add the begin word to queue
+        cost[wordId[beginWord]] = 0; // mark the distance from begin to current word
         while(!q.empty()){
             vector<int> now = q.front();
             q.pop();
-
+            //check the last index whether is the destine index
             int last = now.back();
             if(last == dest){
                 vector<string> tmp;
                 for(int index : now){
+                    //push the all index string to tmp
                     tmp.push_back(idWord[index]);
                 }
                 ret.push_back(tmp);
             }else{
+                //serch the matrix in the row [last] 
                 for(int i = 0; i < edges[last].size(); i++){
-                    int to = edges[last][i];
+                    int to = edges[last][i]; //distance from last to i
+                    //and check if the distance
                     if(cost[last] + 1 <= cost[to]){
                         cost[to] = cost[last] + 1;
-                        vector<int> tmp(now);
-                        tmp.push_back(to);
-                        q.push(tmp);
+                        vector<int> tmp(now);//rebuild the tmp base the now
+                        tmp.push_back(to); //find the path add it 
+                        q.push(tmp); //record it
                     }
                 }
             }
